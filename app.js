e3338b2426db1f541087ecc00b77029667404ec2
Vivek maurya <vivek563maurya@gmail.com>
2023-03-19 20:27:52 +0530

    "added file"

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+//import all routes here
+
+const home = require('./routes/home')
+
+//router middleware
+app.use("/api/v1", home)
+
+
+
+module.exports = app;
---
+++ routes/home.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+const {home} = require('../controllers/homeController');
+
+router.route('/').get(home)
+
+
+
+
+module.exports = router;
